1ad17ac227a5d23b552c173ba57f16aae6368dab
Taelim Yeom <ta050279@gmail.com>
2026-02-21 17:44:40 +0900

    Update annotation directory and device configuration

--- evaluations/st_linear_probing/train_st_conma_ie_dlpfc.py
+++ evaluations/st_linear_probing/train_st_conma_ie_dlpfc.py
@@ -81,12 +81,12 @@ LABEL_NAMES = list(LABEL_MAP.keys())
 
 # Paths
 IMAGE_DIR = "./pt_dataset/st_images"
-ANNOTATION_DIR = "./ft_dataset/DLPFC/DLPFC_annotations"
+ANNOTATION_DIR = "./ft_dataset/spatial_clustering/DLPFC/DLPFC_annotations"
 CHECKPOINT_PATH = "./checkpoints/st_conma_pythia410m_12layers_3aug_clip/checkpoint_epoch_12.pt"
 
 # Model config
 VISION_MODEL_NAME = "pathoduet"
-DEVICE = "cuda:0"
+DEVICE = "cuda:3"
 
 # Training config
 BATCH_SIZE = 32
